f1bebbffb1c6238fadeb4236a08fb59fd866a471
jvelozag-we <83142824+jvelozag-we@users.noreply.github.com>
2023-10-10 22:04:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,11 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "5.39.0" # Reemplaza esto con la versión válida del proveedor
+      version = "5.39.0" 
     }
   }
 }
 
-# Configure the GitHub Provider
-# provider "github" {
-#   token = "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"
-# }
-# Recurso para agregar membresía de usuario a un repositorio
 provider "github" {
   token = "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"
 }
